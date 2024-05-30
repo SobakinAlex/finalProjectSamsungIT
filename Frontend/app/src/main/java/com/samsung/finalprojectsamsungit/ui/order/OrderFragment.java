a13367db667e3e9922f1b6bab6bbd3ef7490402a
SobakinAlex <amsabanov@gmail.com>
2024-05-30 05:19:26 +0300

    lwv

--- Frontend/app/src/main/java/com/samsung/finalprojectsamsungit/ui/order/OrderFragment.java
+++ Frontend/app/src/main/java/com/samsung/finalprojectsamsungit/ui/order/OrderFragment.java
@@ -1,21 +1,48 @@
 package com.samsung.finalprojectsamsungit.ui.order;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.samsung.finalprojectsamsungit.R;
+import com.samsung.finalprojectsamsungit.databinding.FragmentOrderBinding;
+import com.samsung.finalprojectsamsungit.ui.map.MapFragment;
 
 public class OrderFragment extends Fragment {
+    private FragmentOrderBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        OrderViewModel orderViewModel =
+                new ViewModelProvider(this).get(OrderViewModel.class);
+
+        binding = FragmentOrderBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.cl_order, MapFragment.class, null)
+                        .commit();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
